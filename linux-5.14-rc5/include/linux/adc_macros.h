1dbfb879aee8c6e675573c68cee1b82311baeeee
Yuben Yang <ybenyangmds@gmail.com>
2026-03-25 05:13:18 +0100

    fix global macros

--- linux-5.14-rc5/include/linux/adc_macros.h
+++ linux-5.14-rc5/include/linux/adc_macros.h
@@ -6,9 +6,9 @@
 #define _LINUX_ADC_MACROS_H
 
 #define RSWAP_KERNEL_SUPPORT 3
-#define RMGRID_CPU_FREQ 2100 // in MHz
+#define RMGRID_CPU_FREQ 2300 // in MHz
 // # maximum memory supported in KB. For now support 400GB at most.
-#define RMGRID_MAX_MEM (400UL * 1024 * 1024 * 1024)
+#define RMGRID_MAX_MEM (600UL * 1024 * 1024 * 1024)
 
 #define ADC_PROFILE_PF_BREAKDOWN
 // #define ADC_VM
